07ce42b91d525c24e8abfb2ce466aa1c82c32492
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-01 08:44:26 +0100

    add guards to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { AboutComponent } from "./about/about.component";
 import { AddContactComponent } from "./contact/add-contact/add-contact.component";
 import { ContactComponent } from "./contact/contact.component";
 import { EditContactComponent } from "./contact/edit-contact/edit-contact.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { RoleGuard } from "./guards/role.guard";
 import { HomeComponent } from "./home/home.component";
 import { StatusComponent } from "./status/status.component";
+import { UserComponent } from "./user/user.component";
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent },
+  { path: "", component: HomeComponent, canActivate: [AuthGuard] },
+  { path: "about", component: AboutComponent, canActivate: [AuthGuard] },
+  { path: "user", component: UserComponent, canActivate: [RoleGuard] },
+
+  // { path: "user", component: UserComponent },
   {
     path: "contact",
     component: ContactComponent,
@@ -17,6 +23,7 @@ const routes: Routes = [
       { path: "add", component: AddContactComponent },
       { path: "edit/:id", component: EditContactComponent },
     ],
+    canActivate: [AuthGuard],
   },
   {
     path: "access",
