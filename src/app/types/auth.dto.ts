e4f19c39ab550fbe56ebb4e72ada609d48b91a09
wallas5h@gmail.com <wallas5h@gmail.com>
2022-11-01 08:40:59 +0100

    update types

--- src/app/types/auth.dto.ts
+++ src/app/types/auth.dto.ts
@@ -2,3 +2,35 @@ export interface UserLoginDto {
   email: string;
   password: string;
 }
+type roles = "admin" | "user" | "sales" | "guest";
+
+export interface createUserRegisterDto {
+  email: string;
+  password: string;
+}
+export interface UserRegisterDto2 {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  role: roles;
+  isActive: boolean;
+}
+export interface UserModel {
+  id: string;
+  email: string;
+  role: roles;
+  isActive: boolean;
+}
+
+export interface UserResponseEntity {
+  _id: string;
+  email: string;
+  role: string;
+  isActive: boolean;
+}
+export interface UserUpdateRequest {
+  _id: string | null;
+  email: string | null;
+  role: string | null;
+  isActive: boolean | null;
+}
